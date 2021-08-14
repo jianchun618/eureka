b1fb224904bd64eb6663a8a217f3e8a0bdabbfa5
jianchun <jc33618@163.com>
2021-08-14 10:44:50 +0800

    配置调整

--- src/main/java/com/sxyh/EurekaApplication.java
+++ src/main/java/com/sxyh/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.sxyh.eureka;
+package com.sxyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,7 +13,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
