74c307a1218a8002dff4dc66876e5852ceb64874
jianchun <jc33618@163.com>
2021-08-14 10:07:55 +0800

    初始提交

---
+++ src/main/java/com/sxyh/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.sxyh.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author :
+ * @param :Eureka 服务的源代码 --正常工作数可自动，剔除失效节点功能
+ *         用于服务的发布（需打包运行Eureka）
+ *         -- SpringBoot服务的治理方案。
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
